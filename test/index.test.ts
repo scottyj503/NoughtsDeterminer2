7b2cc00d651b1f04dae123317759f28dfef92106
Scott Jones <scottyj@Scotts-MacBook-Pro.local>
2023-04-19 14:49:50 -0600

    failing condition for horizontal success test

--- test/index.test.ts
+++ test/index.test.ts
@@ -1,10 +1,11 @@
 import { expect } from 'chai'
-import { testFunction } from '..'
+import { naughtsDeterminer } from '..'
 
-describe('when calling your tests...', () => {
-  it('should work', () => {
-    const actual = testFunction()
-    const expected = { message: 'it works!' }
+describe('naughts determiner', () => {
+  it('should return a horizontal success', () => {
+    const testArray = ["X", "-", "X", "<>", "-", "-", "O", "<>", "-", "-", "X"]
+    const actual = naughtsDeterminer(testArray)
+    const expected = 1
 
     expect(actual).to.eql(expected)
   })
