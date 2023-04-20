f4bdd542cd603a6ab8c5176e53687d6670c1ffa5
Scott Jones <scottyj@Scotts-MacBook-Pro.local>
2023-04-20 07:48:12 -0600

    O horizontal first row test - red
    - also added context for clarity

--- test/index.test.ts
+++ test/index.test.ts
@@ -2,28 +2,42 @@ import { expect } from 'chai'
 import { naughtsDeterminer } from '..'
 
 describe('naughts determiner', () => {
-  it('should return a X horizontal success', () => {
-    const testArray: string[] = ["X", "-", "X", "<>", "-", "-", "O", "<>", "-", "-", "X"]
-    const actual = naughtsDeterminer(testArray)
-    const expected = 1
+  context("X Horizontal Row", () => {
+    it('should return a X horizontal success', () => {
+      const testArray: string[] = ["X", "-", "X", "<>", "-", "-", "O", "<>", "-", "-", "X"]
+      const actual = naughtsDeterminer(testArray)
+      const expected = 1
 
-    expect(actual).to.eql(expected)
+      expect(actual).to.eql(expected)
+    })
+
+    it('should return a X horizontal success for second row', () => {
+      const testArray: string[] = ["X", "O", "X", "<>", "-", "X", "X", "<>", "-", "X", "O"]
+      const actual = naughtsDeterminer(testArray)
+      const expected = 4
+
+      expect(actual).to.eql(expected)
+
+    })
+    it('should return a X horizontal success for third row', () => {
+      const testArray: string[] = ["X", "O", "X", "<>", "O", "X", "O", "<>", "X", "X", "-"]
+      const actual = naughtsDeterminer(testArray)
+      const expected = 10
+
+      expect(actual).to.eql(expected)
+
+    })
   })
 
-  it('should return a X horizontal success for second row', () => {
-    const testArray: string[] = ["X", "O", "X", "<>", "-", "X", "X", "<>", "-", "X", "O"]
-    const actual = naughtsDeterminer(testArray)
-    const expected = 4
+  context("O Horizontal Row", () => {
+    it("should return success for the first row", () => {
+      const testArray: string[] = ["O", "O", "-", "<>", "-", "-", "X", "<>", "-", "X", "O"]
+      const actual = naughtsDeterminer(testArray)
+      const expected = 2
 
-    expect(actual).to.eql(expected)
+      expect(actual).to.eql(expected)
 
-  })
-  it('should return a X horizontal success for third row', () => {
-    const testArray: string[] = ["X", "O", "X", "<>", "O", "X", "O", "<>", "X", "X", "-"]
-    const actual = naughtsDeterminer(testArray)
-    const expected = 10
 
-    expect(actual).to.eql(expected)
-
+    })
   })
 })
