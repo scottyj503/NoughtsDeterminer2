d80d18ca0c91ddbc511c18c3a9cadeeaeaa20f76
Scott Jones <scottyj@Scotts-MBP.lan>
2023-04-19 21:29:23 -0600

    third row test - green

--- index.ts
+++ index.ts
@@ -44,8 +44,14 @@ const isSuccess = (numArry: number[]): boolean => {
   return returnVal
 }
 
+const numCompare = (a: number, b:number): number => {
+  if (a < b) return -1
+  if (a > b) return 1
+  return 0
+}
+
 const checkHorizontalSuccess = (naughtArry: NaughtIter, mainPosition: string, blankPosition: string): boolean => {
-  const combinedRow = [...naughtArry[mainPosition], ...naughtArry[blankPosition]].sort()
+  const combinedRow: number[] = [...naughtArry[mainPosition], ...naughtArry[blankPosition]].sort(numCompare)
 
   return isSuccess(combinedRow)
 }
@@ -70,9 +76,15 @@ export const naughtsDeterminer = (strArry: string[]): number => {
       if (currStr === 'X') accum.X2.push(indx)
       if (currStr === '-') accum.X2BL.push(indx)
     }
+
+    if (indx > 7) {
+      if (currStr === 'X') accum.X3.push(indx)
+      if (currStr === '-') accum.X3BL.push(indx)
+    }
     return accum
   }, arryReduced)
 
   if (checkHorizontalSuccess(arryReduced, "X1", "X1BL")) return arryReduced.X1BL[0]
   if (checkHorizontalSuccess(arryReduced, "X2", "X2BL")) return arryReduced.X2BL[0]
+  if (checkHorizontalSuccess(arryReduced, "X3", "X3BL")) return arryReduced.X3BL[0]
 }
