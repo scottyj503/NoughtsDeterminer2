2823589c1ad6eab7393692908a187c16d5f298cf
Scott Jones <scottyj@Scotts-MacBook-Pro.local>
2023-04-21 06:06:20 -0600

    added X left vertical row test - red
    - also added two other O horizontal row tests - green

--- test/index.test.ts
+++ test/index.test.ts
@@ -2,7 +2,7 @@ import { expect } from 'chai'
 import { naughtsDeterminer } from '..'
 
 describe('naughts determiner', () => {
-  context('X Horizontal Row', () => {
+  context('X Horizontal success', () => {
     it('should return a X horizontal success', () => {
       const testArray: string[] = ['X', '-', 'X', '<>', '-', '-', 'O', '<>', '-', '-', 'X']
       const actual = naughtsDeterminer(testArray)
@@ -32,6 +32,32 @@ describe('naughts determiner', () => {
       const testArray: string[] = ['O', 'O', '-', '<>', 'O', '-', 'X', '<>', '-', 'X', 'O']
       const actual = naughtsDeterminer(testArray)
       const expected = 2
+
+      expect(actual).to.eql(expected)
+    })
+
+    it('should return success for the second row', () => {
+      const testArray: string[] = ['O', 'X', '-', '<>', 'O', 'O', '-', '<>', 'X', '-', 'O']
+      const actual = naughtsDeterminer(testArray)
+      const expected = 6
+
+      expect(actual).to.eql(expected)
+    })
+
+    it('should return success for the second row', () => {
+      const testArray: string[] = ['O', 'X', '-', '<>', '-', 'O', 'X', '<>', '-', 'O', 'O']
+      const actual = naughtsDeterminer(testArray)
+      const expected = 8
+
+      expect(actual).to.eql(expected)
+    })
+  })
+
+  context('X Vertical success', () => {
+    it('should return a success for the first left row', () => {
+      const testArray: string[] = ['X', 'X', 'O', '<>', '-', 'O', 'X', '<>', 'X', 'O', 'O']
+      const actual = naughtsDeterminer(testArray)
+      const expected = 4
 
       expect(actual).to.eql(expected)
     })
